5d0ddcc8a94db6cdd394e8aedfb626b343b5413c
Usman Hamid <76064494+usmanhamid379@users.noreply.github.com>
2023-12-30 22:53:30 +0500

    updated portfolio

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -6,7 +6,6 @@
 
 export async function POST(req, res) {
   // const { email, subject, message } = await req.json();
-  // console.log(email, subject, message);
   // try {
   //   const data = await resend.emails.send({
   //     from: fromEmail,
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -46,16 +46,20 @@ const HeroSection = () => {
             >
               Hire Me
             </Link>
-            <Link
+            <div
               href="/"
               className={`px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500
-               to-secondary-500 hover:bg-slate-800 text-white mt-3
+               to-secondary-500 hover:bg-slate-800 text-white mt-3 hover:cursor-pointer
                `}
             >
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Download CV
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2"
+              onClick={()=>{
+                window.open(`/cv/usman_cv.pdf`, '_blank')
+              }}
+              >
+                Check CV
               </span>
-            </Link>
+            </div>
           </div>
         </motion.div>
         <motion.div
